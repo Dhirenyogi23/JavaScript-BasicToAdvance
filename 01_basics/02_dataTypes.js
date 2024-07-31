9a881c385a87d3661a3deb4ec4d723d5972da856
Dhirenyogi23 <150579225+Dhirenyogi23@users.noreply.github.com>
2024-07-31 11:56:14 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,39 @@
+"use strict";  // treat all JS code as a new version.
+
+// alert(2 + 2)   // we are using nodejs, not browser.
+
+
+// code should be always readable form.
+// console.log(3 + 3);
+// console.log("Dhiren");
+
+
+// dataTypes
+
+let name = "Dhiren"
+let age  = 23
+let isLogged = true
+let city;
+
+
+/*
+
+number => 2 to power 53
+bigInt
+string => ""
+boolean => true or false
+null => standalone value
+undefined => 
+symbol => unique
+
+*/
+
+
+// objects
+
+console.log(typeof "Dhiren");  // String datatypes
+console.log(typeof 9);         // number datatypes
+console.log(typeof true);      // boolean datatypes
+console.log(typeof null);      // object
+console.log(typeof undefined); // undefined
+
