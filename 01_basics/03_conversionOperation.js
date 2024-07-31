0b15417f72922480c0331da78565165fc78429ab
Dhirenyogi23 <150579225+Dhirenyogi23@users.noreply.github.com>
2024-07-31 16:10:00 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+// let score = 29
+// let score = true
+// let score = null
+// let score = undefined
+
+let score = "First"
+
+// console.log(typeof score);   //  string
+// console.log(typeof(score));  //  string
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);   // number
+// console.log(valueInNumber);          // NaN
+
+
+//**************************************************************************** */
+
+
+let isLoggedIn = null;
+
+let booleanIsLogggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLogggedIn)
+// console.log(booleanIsLogggedIn);
+
+/* 
+    1 =>        true
+    0 =>        false
+    "" =>       false 
+    "Dhiren"=>  true 
+*/
+
+
+//*********************************************************************** */
+
+
+// number to string conversion.
+
+let someNumber = 23;
+console.log(someNumber);       // it print 23
+console.log(typeof someNumber) // it return number
+
+let numberConvertToNumber = String(someNumber);
+console.log(numberConvertToNumber);                 // it print 23
+console.log(typeof numberConvertToNumber);          // it return string
+
+
+
+//***************************************************************************** */
