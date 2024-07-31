88f1e2ce7355949fc29d77d755aabc4a226c2b0b
Dhirenyogi23 <150579225+Dhirenyogi23@users.noreply.github.com>
2024-07-31 17:48:46 +0000

    code update

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,48 @@
+
+// console.log(2 > 1)     //true
+// console.log(2 >= 1)    // true
+// console.log(2 < 1)     // false
+// console.log( 2<= 1)    // false
+// console.log( 2 == 1)   // false
+// console.log( 2 !== 1)   // true
+
+
+//--------------------------------------------------
+
+// console.log("2" > 1);     // true
+// console.log("02" > 1);    // true
+
+
+// console.log(null > 0);     // false 
+// console.log(null >= 0);    // true
+// console.log(null == 0);    // false
+// console.log(null < 0)      // false  
+// console.log(null <= 0)     // true
+
+
+// console.log(undefined > 0);     // false 
+// console.log(undefined >= 0);    // false
+// console.log(undefined == 0);    // false
+// console.log(undefined < 0)      // false  
+// console.log(undefined <= 0)     // false
+
+
+
+// console.log(undefined > null);    // false
+// console.log(undefined >= null);   // false
+// console.log(undefined == null);   // true
+// console.log(undefined < null);    // false
+// console.log(undefined <= null);   // false
+
+
+//----------------------------------------------------------
+
+
+// ===
+
+// console.log(23 === 23)                   // true
+// console.log("23" === 23);                // false
+// console.log("dhiren" === "dhiren");      // true
+// console.log("dhiren" === "hitesh");      // false
+// console.log(undefined === null)          // false
+// console.log("dhiren" === "d")            // false
