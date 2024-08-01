a4702aef369446c9846b16d7d2b58e8c447c9b35
Dhirenyogi23 <150579225+Dhirenyogi23@users.noreply.github.com>
2024-08-01 12:43:17 +0000

    code update

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,48 @@
+
+//+++++++++++++++++++++ Numbers ++++++++++++++++++++++++++++++++++++++
+
+
+// const score = 400;
+// console.log(score);                // 400
+ 
+
+// const balance = new Number(100);
+// console.log(balance);             // [Number: 100]
+// console.log(balance.toString());  // 100
+// console.log(balance.toFixed(2));  // 100.00
+
+
+// const otherNumber = 223.998;
+// console.log(otherNumber.toPrecision(3));    // 224
+// console.log(otherNumber.toPrecision(4));    // 224.0
+// console.log(otherNumber.toPrecision(5));    // 224.00
+
+
+
+// const amount = 100000;
+// console.log(amount.toLocaleString('en-IN'));   // 1,00,000
+
+
+
+
+//+++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);                 // Object [Math] {}
+// console.log(Math.abs(-23));        // 23
+// console.log(Math.round(23.50));    // 24
+// console.log(Math.ceil(4.2));       // highest roundup and print 5
+// console.log(Math.floor(4.9));      // lowest roundup and print 4
+// console.log(Math.min(2,3,1,7,8));  // 1
+// console.log(Math.max(2,3,1,7,8));  // 8
+
+
+// console.log(Math.random())                       
+// console.log((Math.random()* 10 )+ 1);              
+// console.log(Math.floor(Math.random()* 10 )+ 1); 
+
+
+// const max = 20
+// const min = 10
+
+// console.log(Math.floor(Math.random()*(max-min+1)) + min);    // 10 to 20
