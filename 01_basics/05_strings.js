24d0e2805f4f68d35355f752e391e86cf4e21d70
Dhirenyogi23 <150579225+Dhirenyogi23@users.noreply.github.com>
2024-08-01 06:22:41 +0000

    code update

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,72 @@
+// const name = "Dhiren";
+// const age  = 23;
+
+// console.log(`My name is ${name} and i am ${age} years old`);
+
+
+//------------------------------------------------------------------
+
+
+// const gameName = new String("DhirenDN");
+// console.log(gameName)                          // [String: 'DhirenDN']
+// console.log(gameName[0]);                      // D
+// console.log(gameName.__proto__)                // {}
+// console.log(gameName.length);                  // 8
+// console.log(gameName.toUpperCase());           // DHIRENDN
+// console.log(gameName.toLocaleLowerCase())      // dhirendn
+// console.log(gameName.toLocaleUpperCase());     // DHIRENDN
+// console.log(gameName.endsWith('N'));           // true
+// console.log(gameName.charAt(5));               // n
+// console.log(gameName.indexOf('n'));            // 5
+
+
+//---------------------------------------------------------------------------
+
+
+// const newString = gameName.substring(0,6); 
+// console.log(newString)                             //Dhiren
+
+// const anotherString = gameName.slice(-5,6);
+// console.log(anotherString)                          // ren
+
+
+//--------------------------------------------------------------
+
+
+// const str1 = "   Dhiren    ";
+// console.log(str1);                 // it print space also 
+// console.log(str1.trim());          // it remove the space
+
+
+//---------------------------------------------------------------
+
+
+// const url = "https://dhiren.com/dhiren%23yogi";
+//    console.log(url.replace("%23", "-"));              // https://dhiren.com/dhiren-yogi
+//    console.log(url.includes("dhiren"));               // true
+
+
+//------------------------------------------------------------------------------
+
+
+// let username = "dhiren-is-my-name";
+// console.log(username.split('-'));        // [ 'dhiren', 'is', 'my', 'name' ]
+
+
+
+//--------------------------------------------------------------------------
+
+
+// let sentence = "the brown fox jumps over the lazy dog";
+
+// const words = sentence.split(" ")
+// console.log(words[3])                // jumps   
+
+// const char = sentence.split("")
+// console.log(char[8]);                // n
+
+
+
+
+
+
