d47405421c2da616e72104b3b6a30190207f486d
Dhirenyogi23 <150579225+Dhirenyogi23@users.noreply.github.com>
2024-08-01 16:27:11 +0000

    code update

---
+++ 01_basics/07_date_and_time.js
@@ -0,0 +1,34 @@
+
+
+
+// const myDate = new Date();
+
+// console.log(typeof myDate);                    // object
+// console.log(myDate);                           // 2024-08-01T15:42:08.353Z
+// console.log(myDate.toString());                // Thu Aug 01 2024 15:42:50 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString());            // Thu Aug 01 2024
+// console.log(myDate.toISOString())              // 2024-08-01T15:44:30.921Z
+// console.log(myDate.toLocaleDateString())       // 8/1/2024
+// console.log(myDate.toLocaleString())           // 8/1/2024, 3:47:04 PM
+// console.log(myDate.toLocaleTimeString())       // 3:48:06 PM
+// console.log(myDate.toUTCString());             // Thu, 01 Aug 2024 15:48:56 GMT
+// console.log(myDate.getTimezoneOffset())        // 0
+
+//-------------------------------------------------------------------------------
+
+// console.log(myDate.getDate())                    // it print the day of dates.
+// console.log(myDate.getDay())                     // it return the week
+// console.log(myDate.getFullYear())                // it print the current year
+// console.log(myDate.getHours())                   // it print the current hour
+// console.log(myDate.getMilliseconds())            // it print the current milliSecond
+// console.log(myDate.getMinutes())                 // it print the current minute.
+// console.log(myDate.getMonth())                   // it print the current month.
+// console.log(myDate.getSeconds())                 // it print the current second
+
+
+//----------------------------------------------------------------------------------------------------
+
+// const myDate = new Date(2002, 8, 8);       // (month also start with 0. jan = 0, feb = 1,....)
+// console.log(myDate.toDateString())         // it print (Sun Sep 08 2002)
+// console.log(myDate.getDay())               // it print 0 Because it is Sunday
+
