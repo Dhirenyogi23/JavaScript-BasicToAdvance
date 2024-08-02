148a5a6b01a7c64e911367d6319a63553ef1da0d
Dhirenyogi23 <150579225+Dhirenyogi23@users.noreply.github.com>
2024-08-02 06:13:23 +0000

    code update

---
+++ 02_basics/04_object.js
@@ -0,0 +1,80 @@
+
+// const tinderUser = new Object()         // singleton object 
+// const tinderUser = {}                   // non-singleton object
+
+
+
+//-------------------------------------------------------
+
+const tinderUser = {}
+
+tinderUser.id = "123bc"
+tinderUser.name = "sunny"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);                          // { id: '123bc', name: 'sunny', isLoggedIn: false }
+// console.log(Object.keys(tinderUser));             // [ 'id', 'name', 'isLoggedIn' ]
+// console.log(Object.values(tinderUser));           // [ '123bc', 'sunny', false ]
+// console.log(Object.entries(tinderUser));          // [ [ 'id', '123bc' ], [ 'name', 'sunny' ], [ 'isLoggedIn', false ] ]
+// console.log(Object.hasOwnProperty('name'));       //  true
+
+
+//-----------------------------------------------------------------------------------------------------------------
+
+
+const  regularUser = {
+    email : "some@google.com",
+    fullName : {
+        userFullName: {
+        Fname: "Sam",
+        Lname: "airee"
+        }
+    },
+
+}
+
+
+// console.log(regularUser.fullName.userFullName.Fname);              // sam
+
+
+
+//-------------------------------------------------------------------------------
+
+
+const obj1 = {1:"a", 2:"b", 3:"c"}
+const obj2 = {4:"d", 5:"e", 6:"f"}
+
+
+// many times use this syntax.
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3)
+
+//---------------------------------
+
+// less times use syntax
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+// console.log(obj3);
+
+
+//------------------------------------------------------------------------------------
+
+
+const users = [
+    {
+        id: 1,
+        email: "d@google.com"
+    },
+    {
+        id: 2,
+        email: "n@google.com"
+    },
+    {
+        id: 3,
+        email: "y@google.com"
+    },
+]
+
+// console.log(users[1].email) 
