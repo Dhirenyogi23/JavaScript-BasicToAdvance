2196bc25c6cee81408e8294165116b4c7badd9af
Dhirenyogi23 <150579225+Dhirenyogi23@users.noreply.github.com>
2024-08-03 16:01:54 +0000

    code update

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,32 @@
+
+// Immediately Invoked Function Expression (IIFE).
+
+
+// i +++++++++++++ normal function +++++++++++++++++++++
+
+
+
+// (function chai(){
+//     console.log("DB CONNECTION SUCCESSFULLY")
+// })();
+
+
+// (function chai(name){
+//     console.log(`DB CONNECTION SUCCESSFULLY ${name}`)
+// })('Dhiren');
+
+
+// ii +++++++++++++  arrow function  ++++++++++++++++++++++++
+
+
+
+// (() => {
+//     console.log("DB CONNECTION 2");
+// })()
+
+
+// ((name) => {
+//     console.log(`DB CONNECTION 2 ${name}`);
+// })('Batman')
+
+
