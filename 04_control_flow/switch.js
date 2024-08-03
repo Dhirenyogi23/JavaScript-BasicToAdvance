4bfab55a7457eeb2c882b0a750065470af1ab6da
Dhirenyogi23 <150579225+Dhirenyogi23@users.noreply.github.com>
2024-08-03 17:35:29 +0000

    code update

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,73 @@
+
+// switch case syntax..
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//_________________________________________________________________
+
+
+const month = 12
+
+switch(month){
+    
+    case 1:
+        console.log("Jan")
+    break;
+
+    case 2:
+        console.log("Feb")
+    break
+
+    case 3:
+        console.log("Mar")
+    break
+
+    case 4:
+        console.log("April")
+    break
+
+    case 5:
+        console.log("May")
+    break
+
+    case 6:
+        console.log("Jun")
+    break
+
+    case 7: 
+        console.log("July")
+    break
+
+    case 8:
+        console.log("Aug")
+    break
+
+    case 9:
+        console.log("Sep")
+    break
+
+    case 10:
+        console.log("Oct")
+    break
+
+    case 11:
+        console.log("Nov")
+    break
+
+    case 12:
+        console.log("Dec")
+    break
+
+    default : 
+    console.log("sorry This Month is not Available")
+    break
+
+}
