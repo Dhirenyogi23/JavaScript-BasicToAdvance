e6669e9e72cac790ff12c8399aec232e1e66c743
Dhirenyogi23 <150579225+Dhirenyogi23@users.noreply.github.com>
2024-08-04 03:56:51 +0000

    code update

---
+++ 04_control_flow/truthy_falsy.js
@@ -0,0 +1,84 @@
+
+
+/* Truthy Value
+            =>    "0",  'false',  true,  " ",  [],  {},  function(){}
+*/
+
+
+/* Falsy Value
+           =>    null, undefined, NaN, false, BigInt, 0n,  0, -0, "", 
+*/
+
+
+
+// const Email = "d@yogi.ai"
+
+// if(Email){
+//     console.log("Got the user Email")
+// }
+// else{
+//     console.log("Didn't get the user Email")
+// }
+
+
+
+//_________________________________________________________
+
+
+// ++++++++++ for array +++++++++++++++++
+
+
+// const userArray = []
+
+// if(userArray.length === 0){
+//     console.log("empty array");
+// }
+// else{
+//     console.log("you have array")
+// }
+
+
+
+// ++++++++++++++++++  for Object ++++++++++++++++++++
+
+
+// const userObj = {}
+
+// if(Object.keys(userObj).length === 0){
+//     console.log("empty object")
+// }
+// else{
+//     console.log("you have object")
+// }
+
+
+
+//____________________________________________________________________________
+
+
+// Nullish coalescing operator (??): null, undefined
+
+let val1;
+
+// val1 = 5 ?? 10                 // 5
+// val1 = null ?? 15              // 15
+// val1 = undefined ?? 20         // 20
+// val1 = null ?? undefined       // undefined
+// val1 = undefined ?? null       // null
+// val1 = null ?? 50 ?? 100       // 50
+// val1 = 20 ?? null ?? 10        // 20
+
+
+// console.log(val1);
+
+
+//_________________________________________________________________________
+
+
+// Ternary Operator syntax =>    condition ? true : false
+
+
+const IceCreamPrice = 50
+
+IceCreamPrice >= 30 ? console.log("ice cream price is high") : console.log("ice cream price is low")
+
