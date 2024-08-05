00f14a10f9304da8135f22d57cdd15f8de499a05
Dhirenyogi23 <150579225+Dhirenyogi23@users.noreply.github.com>
2024-08-05 05:50:53 +0000

    code update

---
+++ 05_iterations/02.js
@@ -0,0 +1,39 @@
+//_______________________________________________________________________
+
+// While loop
+
+let index = 0
+
+while( index <= 10){
+    // console.log(`Even Numer: ${index}`)
+    
+    index = index+2
+}
+
+
+
+myArr = ["flask", "superman", "batman"]
+let arrIndex = 0;
+
+while(arrIndex < myArr.length){
+    // console.log(myArr[arrIndex])
+    arrIndex++
+}
+
+
+
+//____________________________________________________________________
+
+
+// Do..While Loop
+
+
+let score = 1;
+
+do {
+    console.log(`Score is : ${score}`)
+    score++
+} while (score <= 10);
+
+
+
