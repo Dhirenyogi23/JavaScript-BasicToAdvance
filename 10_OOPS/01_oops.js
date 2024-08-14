278bc6ec7af3717a50b7fc1222fa721c58703887
Dhirenyogi23 <150579225+Dhirenyogi23@users.noreply.github.com>
2024-08-14 06:18:38 +0000

    code update

---
+++ 10_OOPS/01_oops.js
@@ -0,0 +1,41 @@
+
+
+//___________________________________________________________________
+
+
+// object
+
+
+const user = {
+    username: "Dhiren",
+    loggedIn: true,
+    email: "diren@google.com",
+
+    getUserDetail: function(){
+        console.log("got the username database.")
+        console.log(`username: ${this.username}`)
+        console.log(this)
+    }
+}
+
+
+// console.log(user.username)
+// console.log(user["username"])
+
+// user.getUserDetail();
+
+
+
+//______________________________________________________________________
+
+
+
+// class & constructor
+
+
+
+  
+
+
+
+
